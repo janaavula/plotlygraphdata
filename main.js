56aeedb8fb65871287b6c09aa645145eeb6aab7e
Janardhan Avula <jana.avula@gmail.com>
2017-10-31 13:03:49 -0400

    updated with plotly values

--- main.js
+++ main.js
@@ -1,6 +1,46 @@
+function myFunction() {
 
-	TESTER = document.getElementById('tester');
-	Plotly.plot( TESTER, [{
-	x: [1, 2, 3, 4, 5],
-	y: [1, 2, 4, 8, 16] }], {
-	margin: { t: 0 } } );
+
+var thValue = document.getElementById("threshold-value").value
+var janValue = document.getElementById("january").value
+var febValue = document.getElementById("february").value
+var marValue = document.getElementById("march").value
+var aprValue = document.getElementById("april").value
+var mayValue = document.getElementById("may").value
+var juneValue = document.getElementById("june").value
+var julyValue = document.getElementById("july").value
+var augValue = document.getElementById("august").value
+var septValue = document.getElementById("september").value
+var octValue = document.getElementById("october").value
+var novValue = document.getElementById("november").value
+var decValue = document.getElementById("december").value
+
+  TESTER = document.getElementById('tester');
+  var threshold = {
+  x: ['Jan','Feb','Mar','Apr','May','June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  y: [thValue,thValue,thValue,thValue,thValue,thValue,thValue,thValue,thValue,thValue,thValue,thValue],
+  type: 'scatter'
+};
+
+var ogsm = {
+  x: ['Jan','Feb','Mar','Apr','May','June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  y: [janValue,febValue,marValue,aprValue,mayValue,juneValue,julyValue,augValue,septValue,octValue,novValue,decValue],
+  type: 'scatter'
+};
+
+var data = [threshold, ogsm];
+
+var layout = {
+  title: 'OGSM Measurement vs Entity Pairing',
+  xaxis: {
+    title: 'OGSM Measurement'
+  },
+  yaxis: {
+    title: 'Entity Pairing'
+  }
+};
+
+
+Plotly.newPlot(TESTER, data , layout);
+
+  }
